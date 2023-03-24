50571bdcb5031ceaf5bd659d7da3ab6dae1c5ee5
alfred797 <fredadesoga@gmail.com>
2023-03-24 02:37:16 -0700

    a function that checks for uppercase character

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,22 +1,13 @@
-#include "main.h"
-
-/**
-  * main - Entry point
-  *
-  * Return: Always 0
-  */
-int main(void)
-{
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
-	_putchar('\n');
-
-	return (0);
-}
+#ifndef MAIN_H
+#define MAIN_H
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+#endif /* MAIN_H */
